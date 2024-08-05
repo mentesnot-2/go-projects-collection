a78375cb9df5a5ded6e51945fd6440457fc40f23
mentesnot-2 <sibhatmentesnot@gmail.com>
2024-08-06 01:16:50 +0300

    finished fundamentals of

---
+++ fundamentals-of-go-tasks/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+func calculateAverage(grades []float64) float64 {
+	total := 0.0
+	for _, grade := range grades {
+		total += grade
+	}
+	return total / float64(len(grades))
+}
+
+func main() {
+	var name string
+	var numSubjects int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+
+	fmt.Print("Enter the number of subjects: ")
+	_, err := fmt.Scanf("%d\n", &numSubjects) 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	subjects := make([]string, numSubjects)
+	grades := make([]float64, numSubjects)
+
+	for i := 0; i < numSubjects; i++ {
+		fmt.Printf("Enter the name of subject %d: ", i+1)
+		fmt.Scanln(&subjects[i])
+		var grade float64
+		fmt.Printf("Enter the grade for %s: ", subjects[i])
+		_, err := fmt.Scanln(&grade)
+		if err != nil || grade < 0 || grade > 100 {
+			log.Fatalf("Invalid grade for %s: %v", subjects[i], err)
+		}
+		grades[i] = grade
+	}
+
+	average := calculateAverage(grades)
+	fmt.Printf("\nStudent Name: %s\n", strings.ToUpper(name))
+	fmt.Println("Subject Grades: ")
+	for i := 0; i < numSubjects; i++ {
+		fmt.Printf("%s: %.2f\n", subjects[i], grades[i])
+	}
+	fmt.Printf("Average Grade: %.2f\n", average)
+}
